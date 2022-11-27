afec5cfdf0cd63b1ecc0e1316e51e0d1c77327a7
Rizka Dewi A <72384595+RizkaDewiA@users.noreply.github.com>
2022-11-27 12:13:02 +0700

    [Change]Api Url BE

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://5bf5-114-142-171-57.ap.ngrok.io',
+  apiUrl: 'https://f13a-114-142-168-13.ap.ngrok.io',
   tokenKey: 'token',
   role: 'role',
   usertype: 'usertype'
