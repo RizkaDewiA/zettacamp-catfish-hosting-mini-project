707b4b349f7fcd19d5d5057fc8659e8477db7720
Rizka Dewi A <72384595+RizkaDewiA@users.noreply.github.com>
2022-12-01 20:18:57 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://f1cf-114-142-168-11.ap.ngrok.io',
+  apiUrl: 'https://f1cf-114-142-168-11.ap.ngrok.io/',
   tokenKey: 'token',
   user_id: 'user_id',
   role: 'role',
